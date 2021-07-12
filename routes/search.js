8e6af3a5586ad4c1c59503655da3b97d2370a745
tommy081812 <sungkyun@uw.edu>
2021-07-12 13:34:38 +0900

    socket.io added

--- routes/search.js
+++ routes/search.js
@@ -7,23 +7,49 @@ var prices = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', {title:'Express', arr:[], search_value:''});
+    mariadb.query("SELECT DISTINCT dong FROM apart_price_clone ORDER BY dong", function(err, rows, fields) {
+        console.log(rows);
+        res.render('index', {title:'Search Your Apartment',  search_value:'', dong_list:rows});
+
+      });
 });
-router.get('/:search_value', function(req, res, next) {
-    show_value = req.params.search_value;
-    var search_value = '%' + req.params.search_value + '%';
-    mariadb.query("SELECT DISTINCT street_adress, dong, name, size FROM apart_price_clone WHERE name LIKE ? ORDER BY name",[search_value], function(err, rows, fields) {
+
+router.get('/:dong_value/:name_value', function(req, res, next) {
+    dong_value = req.params.dong_value;
+    name_value = req.params.name_value;
+    var search_value = '%' + name_value + '%';
+    var page_num=0;
+    mariadb.query("SELECT DISTINCT street_adress, dong, name FROM apart_price_clone WHERE dong=? AND name LIKE ? ORDER BY name LIMIT ?,20",[dong_value, search_value,page_num], function(err, rows, fields) {
       if (!err) {
-        //var arr = JSON.parse(JSON.stringify(rows));
         arr = rows;
         console.log(rows);
-        res.render('index', {title:'Express', arr:arr, search_value:show_value});
+        res.render('index', {title:'Search Results', arr:arr, search_value:name_value, page_num:page_num, dong_list:[d});
       } else {
         console.log("err: " + err);
         res.send(err);
       }
     });
   });
+
+  router.get('/:dong_value/:name_value/:page_num', function(req, res, next) {
+    var page_num=parseInt(req.params.page_num);
+    dong_value = req.params.dong_value;
+    name_value = req.params.name_value;
+    var search_value = '%' + name_value + '%';
+    mariadb.query("SELECT DISTINCT street_adress, dong, name FROM apart_price_clone WHERE dong=? AND name LIKE ? ORDER BY name LIMIT ?,20",[dong_value, search_value,page_num], function(err, rows, fields) {
+      if (!err) {
+        arr = rows;
+        console.log(rows);
+        res.render('index', {title:'Search Results', arr:arr, search_value:name_value, page_num:page_num, dong_list:[]]});
+      } else {
+        console.log("err: " + err);
+        res.send(err);
+      }
+    });
+  });
+
+
+  /*
   router.get('/:search_value/:name_size', function(req, res, next) {
     var name = req.params.search_value.split('?')[0]
     var size = req.params.search_value.split('?')[1]
@@ -32,14 +58,14 @@ router.get('/:search_value', function(req, res, next) {
         //var arr = JSON.parse(JSON.stringify(rows));
         console.log(rows);
         prices = rows;
-        res.render('index', {title:'Express', arr:arr, search_value:show_value, prices:prices});
+        res.render('index', {title:'Express', arr:arr, search_value:show_value});
       } else {
         console.log("err: " + err);
         res.send(err);
       }
     });
   });
-
+*/
 
 
 module.exports = router;
